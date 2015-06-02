2fde409fb9568e9a026cdbc407b2940bbff333a9
joshuabamboo <joshuaowens011@gmail.com>
2015-06-01 20:33:54 -0400

    comment replace functionality

--- replace.js
+++ replace.js
@@ -1,3 +1,3 @@
-var edited_html = $("body").html().replace(/steven nu.ez/gi, "JavaScript Master Steven Nunez");
+// var edited_html = $("body").html().replace(/steven nu.ez/gi, "JavaScript Master Steven Nunez");
 
-$("body").html(edited_html);
+// $("body").html(edited_html);
